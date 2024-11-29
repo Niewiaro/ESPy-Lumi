64c6124fbd65c4fa28a5d924629e934f1e621db9
Jakub <56446320+Niewiaro@users.noreply.github.com>
2024-11-30 00:53:32 +0100

    chore: move to numpy rfft

--- FFT/fft.py
+++ FFT/fft.py
@@ -18,7 +18,7 @@ import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from scipy.fftpack import fft
+from numpy.fft import rfft
 import time
 from tkinter import TclError
 
@@ -31,6 +31,9 @@ FORMAT = pyaudio.paInt16  # audio format (16-bit integer)
 CHANNELS = 1  # single channel for microphone
 RATE = 44100  # samples per second
 AMPLITUDE_LIMIT = 2048  # limiting amplitude
+
+MIN_FREQ = 400
+MAX_FREQ = 4000
 
 # Initialize PyAudio
 p = pyaudio.PyAudio()
@@ -53,11 +56,11 @@ except Exception as e:
 # ------------ Plot Setup ---------------
 fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 7))
 x = np.arange(0, 2 * CHUNK, 2)  # samples (waveform)
-xf = np.linspace(0, RATE // 2, CHUNK // 2)  # frequencies (spectrum)
+xf = np.linspace(0, RATE // 2, CHUNK // 2 + 1)  # frequencies (spectrum)
 
 # Create line objects
 (line,) = ax1.plot(x, np.zeros(CHUNK), "-", lw=2)
-(line_fft,) = ax2.semilogx(xf, np.zeros(CHUNK // 2), "-", lw=2)
+(line_fft,) = ax2.semilogx(xf, np.zeros(CHUNK // 2 + 1), "-", lw=2)
 
 # Format waveform axes
 ax1.set_title("Audio Waveform")
@@ -67,15 +70,13 @@ ax1.set_ylim(-AMPLITUDE_LIMIT, AMPLITUDE_LIMIT)
 ax1.set_xlim(0, 2 * CHUNK)
 
 ax2.set_xscale("log")  # Ustawienie logarytmicznej skali na osi X
-ax2.set_xlim(400, 4000)  # Ustawienie zakresu od 400 Hz do 4000 Hz
+ax2.set_xlim(MIN_FREQ, MAX_FREQ)  # Ustawienie zakresu od 400 Hz do 4000 Hz
 # Format spectrum axes
 ax2.set_title("Frequency Spectrum")
 ax2.set_xlabel("Frequency (Hz)")
 ax2.set_ylabel("Magnitude")
 # ax2.set_xlim(20, RATE // 2)
 ax2.set_ylim(0, AMPLITUDE_LIMIT)
-
-print("Stream started")
 
 
 def on_close(evt):
@@ -107,8 +108,7 @@ def animate(i):
         line.set_ydata(data_np)
 
         # Compute FFT and update spectrum
-        yf = fft(data_np)
-        magnitude = np.abs(yf[: CHUNK // 2]) / (CHUNK / 2)
+        magnitude = np.abs(rfft(data_np)) / (CHUNK / 2)  # Now returns 1025 samples
         line_fft.set_ydata(magnitude)
 
         frame_count += 1
@@ -127,10 +127,15 @@ def animate(i):
 
 if __name__ == "__main__":
     start_time = time.time()
+    print(f"Stream started at {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
     # Create the animation object with blit=True
     anim = animation.FuncAnimation(
-        fig, animate, interval=1, blit=True, cache_frame_data=False
+        fig, animate, interval=20, blit=True, cache_frame_data=False
     )
     fig.canvas.mpl_connect("close_event", on_close)
-    plt.show()
+    try:
+        plt.show()
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+        on_close()
